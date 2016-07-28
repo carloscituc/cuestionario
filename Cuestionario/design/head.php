5b619da358b0c5c1dcb82e8d7742e39bba1badb5
carloscituc <carlos.cituc@gmail.com>
2016-07-28 13:10:28 -0500

    Cuestionario con diseño
    Correcciones en estilos.css y head.php

--- Cuestionario/design/head.php
+++ Cuestionario/design/head.php
@@ -1,8 +1,8 @@
     <head>
         <meta charset="utf-8">
         <!--Etiquetas para que boostrap funcione-->
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="widt=device-width, initial-scale=1">
+        <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Cuestionario</title>
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/estilos.css" rel="stylesheet">
--- Cuestionario/detalle-E-A.php
+++ Cuestionario/detalle-E-A.php
@@ -22,83 +22,75 @@
             //Contamos el número de preguntas devueltas
             $total2 = count($arrayPregunta);
         ?>
-           
-        <!-- start: Content -->
-        <div id="content" class="article-v1">
-            <div class="col-md-12">
-                <div class="panel">
-                    <div class="panel-body">
-                                <form id="<?php echo $arrayBloque[0]['nombre']; ?> method="post" action="#">
-                                    <div class="form-group">
-                                        <div class="row">
-                                            <div class="col-sm-12" id="titulo">
-                                                <h1 align="center"><?php echo $arrayBloque[0]['nombre']; ?></h1>
-                                            </div>
-                                        </div>
-                                    <?php 
+        
+        <div class="container">
+            <div class="panel">
+                <div class="panel-heading">
+                    <h2><strong><?php echo $arrayBloque[0]['nombre']; ?></strong></h2>
+                </div>
+                <div class="panel-body">
+                    <form class="form-horizontal" id="<?php echo $arrayBloque[0]['nombre']; ?> method="post" action="#">
+                        <?php 
                                         //Esta variable nos servirá para detener la comparacion de si determinada pregunta pertene a un determinado bloque
-                                        $variable = 0;
+                            $variable = 0;
                                         //Iniciamos un ciclo que se repetirá según el número de bloques
-                                        for($i=0; $i < $total; $i++){
-                                    ?>                               
-                                        <div class="row" id="bloque">
-                                            <div class="col-sm-12">
-                                                    <h3>Sección #<?php echo ($i+1) . " " . $arrayBloque[$i]['instruccion']; ?></h3>
-                                             </div>
-                                        </div>
-                                    <?php
-                                        //Iniciamos un ciclo que se repetirá según el número de preguntas
-                                        for($j=$variable; $j < $total2; $j++){
+                            for($i=0; $i < $total; $i++){
+                        ?>  
+                        <div class="bloque">
+                            <h3 class="titulo_bloque"><strong>Sección #<?php echo ($i+1) . "</strong><br> Instucciones: " . $arrayBloque[$i]['instruccion']; ?></h3>
+                            <?php
+                                //Iniciamos un ciclo que se repetirá según el número de preguntas
+                                for($j=0; $j < $total2; $j++){
                                             //Con este if verificamos que si determinada pregunta pertenece al bloque, para imprimirlo dentro de el
-                                            if($arrayBloque[$i]['idBloquePregunta'] == $arrayPregunta[$j]['idBloquePregunta']){
-                                    ?>      
-                                           
-                                    <div class="row" id="pregunta">
-                                                <div class="col-sm-12">
-                                                    <label for="pregunta<?php echo ($j+1); ?>"><?php echo ($j+1). ".- " .$arrayPregunta[$j]['pregunta']; ?></label>
-                                                </div>
-                                            
-                                                    <?php
+                                    if($arrayBloque[$i]['idBloquePregunta'] == $arrayPregunta[$j]['idBloquePregunta']){  
+                            ?>
+                                <div class="form-group pregunta">
+                                    <label class="col-xs-12 titulo_pregunta"><?php echo ($j+1). ".- " .$arrayPregunta[$j]['pregunta']; ?></label>
+                                    <?php
                                                         //Iniciamos un ciclo que se repetirá 10 veces(sólo hay 10 posibles respuestas)
                                                         //eso no implica que tenga 10 respuestas
-                                                        for($k = 1; $k<11; $k++){
+                                            for($k = 1; $k<11; $k++){
                                                             //Se verifica si la respuesta existe y si no, no la imprimimos
-                                                            if(isset($arrayPregunta[$j]['respuesta'.$k])){
-                                                    ?>
-                                                                    <div class="col-sm-12">
-                                                                        <input id="respuesta<?php echo ($j+1); ?>" name="respuesta<?php echo ($j+1);?>" type="radio">
-                                                                        <label for="respuesta<?php echo ($j+1); ?>"><?php echo " " . $arrayPregunta[$j]['respuesta'.$k]; ?></label>
-                                                                    </div><!-- /.col-lg-6 -->
-                                                    <?php
-                                                            }
-                                                        }
-                                                    ?>
-
-                                        </div>
-                                        <br> 
+                                                if(isset($arrayPregunta[$j]['respuesta'.$k])){
+                                    ?>
+                                    <div class="col-xs-12">
+                                        <label class="radio_opcion radio-inline">
+                                            <input type="radio" name="<?php echo $j; ?>"><?php echo " " . $arrayPregunta[$j]['respuesta'.$k]; ?>
+                                        </label>
+                                    </div>
                                     <?php
-                                                }else{
+                                            }
+                                        }
+                                    ?>
+                                </div>
+                            <?php
+                                    
+                                    }/*else{
                                                     //En dado caso que se llegue al número máximo de respuestas para el bloque
                                                     //almacenamos el número de la pregunta del arrayPreguntas
-                                                    $variable = $j;
+                                        $variable = $j;
 
                                                     //Alteramos la variable que controla la impresión de preguntas para terminar su impresión
                                                     //para cada bloque
-                                                    $j = $total2;
-                                                }
-                                            }   
-                                    ?>
-                                    </div>
-                                    <?php
-                                    }                                    
-                                    ?>
-                                </form>
-                            
-                    </div>
-                </div>         
+                                        $j = $total2;
+                                    }
+                                    /*
+                                    echo "TOtal 2: $total2<br>";
+                                    echo "variable: $variable<br>";
+                                    echo "j: $j<br>";*/
+                                }
+                            ?>
+                        </div> 
+                        <?php
+                            }
+                        ?>            
+                    </form>
+                </div>
             </div>
         </div>
-        <?php } ?>
+        <?php 
+        }
+        ?>
         <!-- end: content -->
         <?php include("design/footer.php");?>
     </body>
